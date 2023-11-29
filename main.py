45e1bfd7474c932a243be7e4ef66ce6eec261d1b
benhuangbmj <120871839+benhuangbmj@users.noreply.github.com>
2023-11-29 04:44:53 +0000

    tested the API

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import requests
+import os
+API_TOKEN = os.environ['Hugging_Face_Token']
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+API_URL = "https://api-inference.huggingface.co/models/bigscience/bloom"
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+data = query({"inputs": "javascript is"})
+print(data)
