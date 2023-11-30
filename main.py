1bf9d1dd57abdd95530a98c37a4c6e0f8f36f782
benhuangbmj <120871839+benhuangbmj@users.noreply.github.com>
2023-11-30 17:26:20 +0000

    tested

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import requests
 import os
 API_TOKEN = os.environ['Hugging_Face_Token']
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
-API_URL = "https://api-inference.huggingface.co/models/bigscience/bloom"
+API_URL = "https://api-inference.huggingface.co/models/gpt2-large"
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
+    print(response)
     return response.json()
-data = query({"inputs": "javascript is"})
-print(data)
+data = query({
+  "inputs": "dilemma means", 
+  "parameters": {
+    "max_new_tokens": 250
+  }
+})
+for i in range(len(data)):
+  print(data[i]['generated_text'])
+    
