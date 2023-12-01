dddeb9edd94df9dcd75477e7574961a1e189b8eb
benhuangbmj <120871839+benhuangbmj@users.noreply.github.com>
2023-12-01 04:28:13 +0000

    tested langchain

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-import requests
+
+#import requests
 import os
-API_TOKEN = os.environ['Hugging_Face_Token']
+from langchain.llms import HuggingFaceHub
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.schema import HumanMessage
+
+API_TOKEN = os.environ['HUGGINGFACEHUB_API_TOKEN']
+
+hub_llm = HuggingFaceHub(repo_id='gpt2')
+prompt = PromptTemplate(
+  input_variables=["question"],
+  template = "Translate English to SQL: {question}"
+)
+#hub_chain = LLMChain(prompt=prompt, llm=hub_llm, verbose=True)
+#print(hub_chain.run("What is the average age of the respondents using a mobile device?"))
+
+text='What would be a good company name for a company that makes colorful socks?'
+messages=[HumanMessage(content=text)]
+print(hub_llm.invoke(messages))
+'''
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 API_URL = "https://api-inference.huggingface.co/models/gpt2-large"
 def query(payload):
@@ -15,4 +34,4 @@ data = query({
 })
 for i in range(len(data)):
   print(data[i]['generated_text'])
-    
+'''
